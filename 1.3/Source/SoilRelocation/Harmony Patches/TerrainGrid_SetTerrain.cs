9d51ea2fdf51a5f6f9ef4e8663d931bab59bce62
Evelyn <udderlyevelyn@gmail.com>
2022-03-04 21:49:35 -0500

    Added null check for newTerr.placeHolders to SetTerrain postfix.

--- 1.3/Source/SoilRelocation/Harmony Patches/TerrainGrid_SetTerrain.cs
+++ 1.3/Source/SoilRelocation/Harmony Patches/TerrainGrid_SetTerrain.cs
@@ -23,7 +23,7 @@ namespace SR
         {
             if (__state.Item1 == newTerr || __state.Item1 == null) //If we're not actually changing anything or it had no terrain previously..
                 return; //Who cares?
-            if (newTerr.placeWorkers.Contains(typeof(PlaceWorker_Soil)) && __state.Item2) //If the new terrain is one of the soils we care about and there was no underTerrain before..
+            if (__state.Item2 && (newTerr.placeWorkers != null && newTerr.placeWorkers.Contains(typeof(PlaceWorker_Soil)))) //If the new terrain is one of the soils we care about and there was no underTerrain before..
                 ___map.terrainGrid.SetUnderTerrain(c, __state.Item1); //Store the old terrain as underTerrain.
         }
     }
